4a6066a9d50ff5f19e0f7f79e858055589cf8e8c
grodnane <gustavodaniel.rod@gmail.com>
2023-05-16 08:56:48 -0300

    added find by id and delete by id

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,34 @@
-const getAllTasks = (req, res) => {
-  res.send("all items from the file");
+const Task = require("../models/task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const allTasks = await Task.find({});
+    res.status(200).json({ allTasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTask = (req, res) => {
-  res.send(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const getTaskById = (req, res) => {
-  res.json({ id: req.params.id });
+
+const getTaskById = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOnnpmne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const updateTaskById = (req, res) => {
   res.send("update task by id");
--- models/task.js
+++ models/task.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
-  date: Date,
+  name: {
+    type: String,
+    required: [true, "Name must be provided."],
+    trim: true,
+    unique: true,
+    maxlength: [20, "Name can not be more than 20 characters"],
+  },
+  completed: { type: Boolean, default: false },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
